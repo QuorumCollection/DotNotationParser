537aed44b9d09e84b5e7c8810ae4023f6fffde3e
Jesse Donat <donatj@gmail.com>
2025-04-12 17:45:39 -0500

    Add types to ParseException class

--- src/Exceptions/ParseException.php
+++ src/Exceptions/ParseException.php
@@ -7,9 +7,12 @@ class ParseException extends \InvalidArgumentException {
 	public const CODE_UNEXPECTED_CHARACTER = 22;
 	public const CODE_UNEXPECTED_EOF       = 484;
 
+	/**
+	 * @var int The index of the character that caused the exception
+	 */
 	private $charIndex;
 
-	public function __construct( $message, int $charIndex, $code, ?\Throwable $previous = null ) {
+	public function __construct( string $message, int $charIndex, int $code, ?\Throwable $previous = null ) {
 		parent::__construct($message, $code, $previous);
 
 		$this->charIndex = $charIndex;
